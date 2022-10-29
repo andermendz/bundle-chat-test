c408320b057cfcd8947cfb856da595453cf399cb
Anderson Mendoza <74158361+andermendz@users.noreply.github.com>
2022-10-29 07:55:54 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 const cookies = new Cookies();
 
-const apiKey = 'db8gcervnmvw';
+const apiKey = 'rmx3mbmt74yc';
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
